75822b4257de50a643b530e91d2b2ded0fa15eea
J.-S. Raulin <jeanseb.raulin@gmail.com>
2025-12-05 19:21:41 +0100

    Ajout de la projection finale pour l'attention (oubli)

--- src/cmbrtAttention.py
+++ src/cmbrtAttention.py
@@ -24,6 +24,7 @@ class CmbrtAttention(nn.Module):
         self.value = nn.Linear(config.hidden_size, self.all_head_size)
 
         self.dropout = nn.Dropout(config.attention_probs_dropout_prob)
+        self.output = nn.Linear(config.hidden_size, config.hidden_size)
 
     def transpose_for_scores(self, x):
         new_x_shape = x.size()[:-1] + (self.num_attention_heads, self.attention_head_size)
@@ -57,6 +58,7 @@ class CmbrtAttention(nn.Module):
         context_layer = torch.matmul(attention_probs, value_layer)
         context_layer = context_layer.permute(0, 2, 1, 3).contiguous()
         new_context_layer_shape = context_layer.size()[:-2] + (self.all_head_size,)
-        context_layer = context_layer.view(*new_context_layer_shape)
 
-        return context_layer
+        context_layer = context_layer.view(*new_context_layer_shape)
+        output = self.output(context_layer) # Projection finale
+        return output
