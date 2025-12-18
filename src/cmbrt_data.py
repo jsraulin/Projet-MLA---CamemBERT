6537e073d8147be7e5491bd4aa262efb579244b9
J.-S. Raulin <jeanseb.raulin@gmail.com>
2025-12-18 15:02:06 +0100

    We use a SentencePiece tokenizer we train on a part (all of it since we've only got about 6Go of dataset) of the pre-train dataset.

--- src/cmbrt_data.py
+++ src/cmbrt_data.py
@@ -1,19 +1,77 @@
 import pytorch_lightning as pl
-from torch.utils.data import DataLoader, Dataset
-from transformers import CamembertTokenizer
-
-#TODO: create DataSet class that tokenizes the data (fr_part1, 2, 3), takes (train_data, tokenizer).
+from torch.utils.data import DataLoader
+from transformers import PreTrainedTokenizerFast, DataCollatorForLanguageModeling
+from datasets import load_dataset
+from itertools import chain
 
 class CmbrtDataModule(pl.LightningDataModule):
-    def __init__(self, batch_size: int = 16):
+    def __init__(self, 
+                 train_files: list, 
+                 tokenizer_dir: str = ".", 
+                 batch_size: int = 8, 
+                 max_length: int = 512):
         super().__init__()
+        self.train_files = train_files
+        self.tokenizer_dir = tokenizer_dir
         self.batch_size = batch_size
-        self.tokenizer = CamembertTokenizer.from_pretrained("camembert-base") # TODO: change, make own tokenizer based on SentencePiece
+        self.max_length = max_length
 
     def setup(self, stage=None):
-        # TODO: self.train_dataset = DataSet(data, self.tokenizer) classused to train
-        pass
+        # Charge le Tokenizer entraîné
+        self.tokenizer = PreTrainedTokenizerFast.from_pretrained(
+            self.tokenizer_dir,
+            model_max_length=self.max_length
+        )
+        # Il faut définir manuellement les tokens spéciaux car save_model ne garde que le vocabulaire
+        self.tokenizer.bos_token = "<s>"
+        self.tokenizer.eos_token = "</s>"
+        self.tokenizer.unk_token = "<unk>"
+        self.tokenizer.pad_token = "<pad>"
+        self.tokenizer.mask_token = "<mask>"
+
+        # Chargement des données
+        dataset = load_dataset("text", data_files={"train": self.train_files})
+
+        # Tokenisation
+        def tokenize_function(examples):
+            # SentencePiece gère le texte brut directement
+            return self.tokenizer(examples["text"], return_special_tokens_mask=True)
+
+        tokenized_datasets = dataset.map(
+            tokenize_function,
+            batched=True,
+            remove_columns=["text"],
+            num_proc=4
+        )
+
+        # Grouping pour remplir les séquences de 512
+        # L'article dit : "We enforce each sequence to only contain complete paragraphs"
+        def group_texts(examples):
+            concatenated_examples = {k: list(chain(*examples[k])) for k in examples.keys()}
+            total_length = len(concatenated_examples[list(examples.keys())[0]])
+            if total_length >= self.max_length:
+                total_length = (total_length // self.max_length) * self.max_length
+            result = {
+                k: [t[i : i + self.max_length] for i in range(0, total_length, self.max_length)]
+                for k, t in concatenated_examples.items()
+            }
+            return result
+
+        self.train_dataset = tokenized_datasets["train"].map(
+            group_texts,
+            batched=True,
+            num_proc=4
+        )
 
     def train_dataloader(self):
-        # return DataLoader(self.train_dataset, batch_size=self.batch_size, shuffle=True)
-        pass
+        return DataLoader(
+            self.train_dataset,
+            batch_size=self.batch_size,
+            shuffle=True,
+            collate_fn=DataCollatorForLanguageModeling(
+                tokenizer=self.tokenizer, 
+                mlm=True, 
+                mlm_probability=0.15
+            ),
+            pin_memory=True
+        )
